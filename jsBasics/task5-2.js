e917ca8f731cfddeaaf9b3659d535766265ea9e2
Olena Plaskonis <olena.plaskonis@gmail.com>
2024-04-30 16:46:59 +0300

    homework 3

---
+++ jsBasics/task5-2.js
@@ -0,0 +1,7 @@
+const log = console.log;
+
+var length = 45;
+var width = 200;
+var square = length*width;
+
+log('Площа прямокутника -', square.toFixed(2))
---
+++ jsBasics/task5-3.js
@@ -0,0 +1,7 @@
+const log = console.log;
+
+var radius = 13;
+var height = 42
+var vol = Math.PI*Math.pow(radius,2)*height;
+
+log('Об"єм циліндра -', vol.toFixed(2))
