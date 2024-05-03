6592b8594c312996ce55af0329d9455ef41aeeda
Olena Plaskonis <olena.plaskonis@gmail.com>
2024-05-03 12:06:05 +0300

    5.1. JavaScript conditions and loops

---
+++ jsCond/task3.js
@@ -0,0 +1,13 @@
+const log = console.log;
+
+const number = 6;
+
+for (let i = 1; i <= 10; i++) {
+    log (`${number} * ${i} = ${number * i}`);
+}
+
+let n = 1; 
+while (n <= 10) {
+    log (`${number} * ${n} = ${number * n}`);
+    n++;
+}
